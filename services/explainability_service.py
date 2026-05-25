24f2beb6b1c5363e85185446c583fcaab5edeb7a
Himanshu Yadav <168503095+Himanshu-2678@users.noreply.github.com>
2026-05-25 09:26:07 +0530

    Add rule-based explainability layer for anomaly predictions

--- services/explainability_service.py
+++ services/explainability_service.py
@@ -9,15 +9,15 @@ def generate_anomaly_reasons(feature_vector):
     reasons = []
 
     if feature_vector["velocity_1m"] > VELOCITY_THRESHOLD:
-        reasons.append("high_transaction_velocity")
+        reasons.append("High Transaction Velocity")
 
     if feature_vector["avg_amount_1m"] > AVG_AMOUNT_THRESHOLD:
-        reasons.append("high_average_transaction_amount")
+        reasons.append("High Average Transaction Amount")
 
     if feature_vector["merchant_diversity_1m"] > MERCHANT_DIVERSITY_THRESHOLD:
-        reasons.append("high_merchant_diversity")
+        reasons.append("High Merchant Diversity")
 
     if feature_vector["amount"] > LARGE_TRANSACTION_THRESHOLD:
-        reasons.append("large_transaction_amount")
+        reasons.append("Large Transaction Amount")
 
     return reasons
